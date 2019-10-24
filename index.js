6999f22e3d26eef948479d7e9c6abfab586f48db
Joshua McKinney <mckinneyj@byui.edu>
2019-10-23 19:46:21 -0600

    added csv and mapping of course codes

--- index.js
+++ index.js
@@ -18,12 +18,29 @@ var subAccounts = [
         id: 44
     }
 ],
-    // var term = ""
     terms = [{
-        name: "Summer 2019",
-        id: 91
+        name: "Fall 2019",
+        id: 89
     }];
 
+function fixCode(code) {
+    return code.toLowerCase().replace(/\s/g, '');
+}
+
+var tempCourseCode = {
+    "AGBUS 105": "AGBPC 105",
+    "BUS 115": "BUSPC 115",
+    "CS 101": "CSPC 101",
+    "FAML 160": "FAMPC 160",
+    "FAML 498R": "",
+    "SI 250": "",
+    "HTMBC 110": "HTMPC 110",
+    "REL 261": "RELPC 261",
+    "SMMBC 105": "SMMPC 105",
+    "TESOL 101": "TESPC 101",
+    // "WDD 100": "WDDPC 100"
+}
+badCourseCodes = Object.keys(tempCourseCode).map(fixCode);
 
 async function getGroupCategories(courseId) {
     function sortOnName(a, b) {
@@ -63,11 +80,12 @@ async function getGroupCategories(courseId) {
 }
 
 function semesterBlueprintSIStoMasterSIS(BpSIS) {
+
     // remove 
     //    2019
     //    spring
     //    number from block ()
-    // Replace
+    // Replac e
     // 	Blueprint with Master
 
     // FAML 120.Initiative.None.2019.Spring.Block1.Blueprint
@@ -83,8 +101,24 @@ function semesterBlueprintSIStoMasterSIS(BpSIS) {
     // BA 211.Initiative.None.Block.Master
     //    BA 211.Initiative.None.Block.Master
     var [courseCode, init, none, year, semester, block, blueprint] = BpSIS.split('.');
-    // remove the number from the block if it has one
-    block = block.replace(/\d/g, '');
+    if (courseCode === "FAML 498R") {
+        return "FAML 498R.Internship-Project.None.None.Master";
+    }
+    if(BpSIS === "SI 250.Gathering.2019.Fall.Blueprint"){
+        return "SI 250.--.None.None.Master";
+    }
+    try{
+        // remove the number from the block if it has one
+        block = block.replace(/\d/g, '');
+    }catch(error){
+        console.error(error);
+        console.log(chalk.yellow(BpSIS));
+    }
+
+    var localTempCourseCode = tempCourseCode[courseCode];
+    if (localTempCourseCode !== undefined) {
+        courseCode = localTempCourseCode;
+    }
     // send it back
     // had to hard code Block For the summer courses
     // return [courseCode, init, none, "Block", "Master"].join('.');
@@ -127,7 +161,12 @@ async function getGroupData(course, i) {
         // are the blueprint and the section the same?
         course.sameGroupsSemester = course.parentGroupCategories === course.groupCategories;
         // get the mater course id
-        course.masterSISId = semesterBlueprintSIStoMasterSIS(course.semesterBpSISId);
+        try {
+
+            course.masterSISId = semesterBlueprintSIStoMasterSIS(course.semesterBpSISId);
+        } catch (error) {
+            console.error(error);
+        }
         // get the groups from the masterCourse
         course.masterGroupsCategories = await getGroupCategories(`sis_course_id:${course.masterSISId}`);
         // are the Master and the section the same?
@@ -146,37 +185,83 @@ function makeFileName(terms, subAccounts) {
     return `${termNames}+${subAccountNames}+GroupReport_${Date.now()}.csv`
 }
 
+ async function getCourses(){
+    var term,
+    courses = [];
+
+    for (let j = 0; j < terms.length; j++) {
+        term = terms[j];
+        for (let i = 0; i < subAccounts.length; i++) {
+            const subAccount = subAccounts[i];
+            let coursesI = await canvas.get(`/api/v1/accounts/${subAccount.id}/courses?sort=sis_course_id&order=asc&search_by=course&include%5B%5D=subaccount&enrollment_term_id=${term.id}&include[]=term`)
+            courses = courses.concat(coursesI);
+        }
+    }
+    printPretty(courses[0]);
+   return courses
+            .filter(course => {
+                return terms.some(term => term.id === course.term.id);
+            })
+            .filter(course => {
+                return subAccounts.some(subAccount => subAccount.id === course.account_id);
+            });
+}
+
+async function getCoursesCSV(){
+    async function getCSV(courseCsvFile) {
+        const stripBOM = require('strip-bom');
+        const dsv = require('d3-dsv');
+        const path = require('path');
+        const fs = require('fs');
+        //resolve the path the user puts in
+        courseCsvFile = path.resolve(courseCsvFile);
+        //read it in and remove the potential BOM and then parse with DSV 
+        var csvCourseData = dsv.csvParse(stripBOM(fs.readFileSync(courseCsvFile, 'utf8')));
+        return csvCourseData;
+    }
+    
+    async function getCourseFromSection(courseIn){
+        var section =  await canvas.get(`/api/v1/sections/sis_section_id:${courseIn.sisID}`);
+        return  await canvas.get(`/api/v1/courses/${section.course_id}?include%5B%5D=subaccount&include[]=term`);
+    }
+    
+    var courses = await getCSV(`./tempList.csv`);
+    
+    courses = await pMap(courses,getCourseFromSection,{concurrency: 10});
+    return courses;
+
+}
+
 async function main() {
     try {
         var term,
             courses = [];
 
-        for (let j = 0; j < terms.length; j++) {
-            term = terms[j];
-            for (let i = 0; i < subAccounts.length; i++) {
-                const subAccount = subAccounts[i];
-                let coursesI = await canvas.get(`/api/v1/accounts/${subAccount.id}/courses?sort=sis_course_id&order=asc&search_by=course&include%5B%5D=subaccount&enrollment_term_id=${term.id}&include[]=term`)
-                courses = courses.concat(coursesI);
-            }
-        }
+
 
         //get all the courses we need
         // pretty(pathwayCourses[0]);
         //filter for winter term
 
-        // pretty(courses[0]);
-        var courses = courses
-            .filter(course => {
-                return terms.some(term => term.id === course.term.id);
-            })
+        
+        courses = await getCoursesCSV();
+        printPretty(courses[0]);
+        
+        courses = courses 
             .map(course => ({
                 name: course.name,
                 id: course.id,
                 sis: course.sis_course_id,
                 term: course.term.name,
                 start: course.start_at,
-                end: course.end_at
+                end: course.end_at,
+                code: fixCode(course.course_code)
             }))
+            // .filter(course => !badCourseCodes.includes(course.code))
+            // .filter(course => badCourseCodes.includes(course.code))
+            // .filter(course => course.code === "si250")
+            // .filter(course => course.code.includes("cit"))
+            // .filter(course => course.sis === "Online.2019.Fall.CIT 261.thayneti@byui.edu.6")
             .sort((a, b) => {
                 if (a.sis < b.sis) {
                     return -1;
@@ -199,7 +284,7 @@ async function main() {
 
         //loop them to get their group data
         courses = await pMap(courses, getGroupData, {
-            concurrency: 5
+            concurrency: 10
         });
 
         var fileName = makeFileName(terms, subAccounts);
@@ -209,7 +294,7 @@ async function main() {
         console.log(`Wrote ${fileName}`);
 
     } catch (error) {
-        console.error(chalk(error));
+        console.error(chalk.red(error.stack));
     }
 }
 
